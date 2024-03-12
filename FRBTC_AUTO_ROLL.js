c058a233331e4527ec8db8ddb733be2a34955c1a
kseryszko <89846710+kseryszko@users.noreply.github.com>
2024-03-12 15:52:59 +0100

    Update FRBTC_AUTO_ROLL.js

--- FRBTC_AUTO_ROLL.js
+++ FRBTC_AUTO_ROLL.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @version      5.11
+// @version      5.00
 // @match        https://freebitco.in/*
 // @match        http*://www.google.com/recaptcha/*
 // @name         Freebitco.in AUTO ROLL (V5.11)
@@ -19,7 +19,7 @@ var reward = {};
         reward.points = parseInt($('.user_reward_points').text().replace(',',""));
         reward.bonustime = {};
         if ($("#bonus_container_free_wof").length != 0) {
-//        if ($("#bonus_container_free_points").length != 0) {
+//        if ($(""#bonus_container_free_points"").length != 0) {
             reward.bonustime.text = $('#bonus_span_free_points').text();
             reward.bonustime.hour = parseInt(reward.bonustime.text.split(":")[0]);
             reward.bonustime.min = parseInt(reward.bonustime.text.split(":")[1]);
@@ -31,39 +31,12 @@ var reward = {};
         if (reward.bonustime.current !== 0) {
             console.log(reward.bonustime.current);
         } else {
-            if (reward.points < 100720) {
+            if (reward.points < 4500) {
                 console.log("waiting for points");
             }
-            else if (reward.points < 1440) {
-//            else if (reward.points < 120) {
-                    console.log("waiting for points 60");
-                RedeemRPProduct('free_wof_1');
-//                RedeemRPProduct('free_points_1');
-                }
-            else if (reward.points < 2160) {
-//            else if (reward.points < 600) {
-                    console.log("waiting for points 120");
-                RedeemRPProduct('free_wof_2');
-//                RedeemRPProduct('free_points_10');
-                }
-            else if (reward.points < 2880) {
-//            else if (reward.points < 600) {
-                    console.log("waiting for points 120");
-                RedeemRPProduct('free_wof_2');
-//                RedeemRPProduct('free_points_10');
-                }
-            else if (reward.points < 3600) {
-//            else if (reward.points < 1200) {
-                    console.log("waiting for points 600");
-                RedeemRPProduct('free_wof_2');
-//                RedeemRPProduct('free_points_50');
-                }
             else {
-                RedeemRPProduct('free_wof_2');
-//                RedeemRPProduct('free_points_100');
+                RedeemRPProduct('free_wof_5');
             }
-//            if ($('#bonus_span_free_lott').length === 0)
-//               RedeemRPProduct('free_lott_100');
         }
     };
     setTimeout(reward.select,1000);
@@ -71,9 +44,9 @@ var reward = {};
 $(document).ready(function(){
     console.log("Status: Page loaded.");
     setTimeout(function(){
-        $('#free_play_form_button').click();
+//        $('#free_play_form_button').click();
         console.log("Status: Button ROLL clicked.");
-    }, random(20000,25000));
+    }, random(2000,4000));
     setInterval(function(){
         console.log("Status: Elapsed time " + count_min + " minutes");
         count_min = count_min + 1;
@@ -84,13 +57,14 @@ $(document).ready(function(){
     }, random(12000,18000));
 //    setInterval(function(){
 //        $('#free_play_form_button').click();
-//        console.log("Status: Button ROLL clicked again.");
-//    }, random(3605000,3615000));
+//        console.log(""Status: Button ROLL clicked again."");
+//   }, random(3605000,3615000));
 });
 function random(min,max){
    return min + (max - min) * Math.random();
 }
     setTimeout(function() {
-    document.getElementById('logo_span_container').innerHTML = ' RP = ' + reward.points;
+    document.getElementById('logo_span_container').innerHTML = ' Glowne|RP=' + reward.points;
     }, 2000);
+
 })();
